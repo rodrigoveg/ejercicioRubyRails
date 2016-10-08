2e3be4a6e4e128bc8f4e580e6799b4de43acd109
Rodrigo Vega <jr_veg@hotmail.com>
2016-10-07 20:15:29 -0500

    add names array

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def index
+    @names = ['name1', 'name2', 'name3', 'name4', 'name5',
+              'name6', 'name7', 'name8', 'name9', 'name10',]
   end
 
   def about
