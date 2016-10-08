c8adf15374abc093b6fade309725a641fea95fb2
Rodrigo Vega <jr_veg@hotmail.com>
2016-10-07 21:19:14 -0500

    add link to about page

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,8 @@
 class PagesController < ApplicationController
+    before_action :set_data
+
   def index
+    @names = ['name1', 'name2', 'name3', 'name4', 'name5', 'name6', 'name7', 'name8','name9', 'name10',  ]
   end
 
   def about
@@ -7,4 +10,10 @@ class PagesController < ApplicationController
 
   def contact
   end
+end
+
+private
+
+def set_data
+  @data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 end
